867a78f5bc83c5c6f18641da022f891e786d8df9
hustdog <hustzhangx@gmail.com>
2023-10-15 00:48:04 +0800

    update

--- od/二叉树遍历.py
+++ od/二叉树遍历.py
@@ -22,3 +22,46 @@ a{b{d,e{g,h{,i}}},c{f}}
 output
 dbgehiafc
 '''
+class TreeNode:
+    def __init__(self, data):
+        self.val = data
+        self.left = None
+        self.right = None
+
+def create_tree_node(input_str):
+    stack = []
+    root = None
+    current_node =None
+    is_left_child =False
+
+    for char in input_str:
+        if char == '{':
+            stack.append(current_node)
+            is_left_child = True
+        elif char == ',':
+            is_left_child = False
+        elif char == '}':
+            stack.pop()
+        else:
+            current_node = TreeNode(char)
+            if root is None:
+                root = current_node
+            else:
+                if is_left_child:
+                    stack[-1].left = current_node
+                else:
+                    stack[-1].right =current_node
+    return root
+
+def inorder(root, res):
+    if root is None:
+        return
+    inorder(root.left, res)
+    res.append(root.val)
+    inorder(root.right, res)
+
+input_str = input()
+root = create_tree_node(input_str)
+res = []
+inorder(root, res)
+print(''.join(res))
